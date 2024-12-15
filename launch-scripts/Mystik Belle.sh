94bada9f41613bbe735beffe91c7f95107496be0
t0b10 <t0b10@null.void>
2024-12-14 20:08:14 -0700

    Added launch scripts. Added duke3dawo log for Knulli.

---
+++ launch-scripts/Mystik Belle.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+
+get_controls
+
+export PORT_32BIT="Y"
+
+
+
+
+
+
+$ESUDO chmod 666 /dev/tty0
+$ESUDO chmod 666 /dev/tty1
+printf "\033c" > /dev/tty0
+printf "\033c" > /dev/tty1
+
+GAMEDIR="/$directory/ports/mystikbelle"
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+LIBDIR="$GAMEDIR/lib32"
+BINDIR="$GAMEDIR/box86"
+
+cd $GAMEDIR
+
+# gl4es
+if [ -f "${controlfolder}/libgl_${CFW_NAME}.txt" ]; then 
+  source "${controlfolder}/libgl_${CFW_NAME}.txt"
+else
+  source "${controlfolder}/libgl_default.txt"
+fi
+
+# system
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$LIBDIR:/usr/lib32:/usr/local/lib/arm-linux-gnueabihf/"
+
+# box86
+export BOX86_ALLOWMISSINGLIBS=1
+export BOX86_LD_LIBRARY_PATH="$LIBDIR"
+
+if [ "$LIBGL_FB" != "" ]; then
+export SDL_VIDEO_GL_DRIVER="$GAMEDIR/gl4es/libGL.so.1"
+fi 
+
+bind_directories ~/.config/Mystik_Belle /$GAMEDIR/conf/
+
+$GPTOKEYB "box86" -c "$GAMEDIR/mystikbelle.gptk" &
+echo "Loading, please wait... (might take a while!)" > /dev/tty0
+$GAMEDIR/box86/box86 $GAMEDIR/runner
+
+$ESUDO kill -9 $(pidof gptokeyb)
+$ESUDO systemctl restart oga_events &
+printf "\033c" > /dev/tty1
+printf "\033c" > /dev/tty0
---
+++ launch-scripts/Tomb Raider 1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+source $controlfolder/tasksetter
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+GAMEDIR="/$directory/ports/tombraider1"
+
+cd $GAMEDIR
+
+$ESUDO chmod 666 /dev/tty1
+
+$ESUDO ./oga_controls OpenLara $param_device &
+bind_directories ~/.openlara $GAMEDIR/conf/.openlara/
+
+$ESUDO $controlfolder/oga_controls OpenLara $param_device &
+SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig" ./OpenLara 2>&1 | tee $GAMEDIR/log.txt
+$ESUDO kill -9 $(pidof oga_controls)
+$ESUDO systemctl restart oga_events &
+printf "\033c" >> /dev/tty1
+
+
